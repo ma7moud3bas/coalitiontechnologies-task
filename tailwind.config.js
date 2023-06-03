5e2385480cc7e4f9282d0a15f498c4cffce8c28d
mahhmoud-enzyme <mahmoud@enzyme.so>
2023-06-03 15:02:20 +0200

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,html}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Bebas: ['Bebas Neue', 'sans-serif'],
+        Lato: ['Lato', 'sans-serif'],
+        Oswald: ['Oswald', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          "white": "#fefefe",
+          "slate-gray": "#6e7e99",
+          "outer-space": "#3E4C45",
+          "charcoal": "#2E4653",
+          "police-blue": "#414F6B",
+        }
+      }
+    },
   },
   plugins: [],
 }
